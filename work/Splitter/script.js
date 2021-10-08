1ea587375fc80f19173b1322d17d7b70577803e0
DaleWegerle <tarryncwegerle@gmail.com>
2021-10-08 12:27:16 +0200

    Tip Logic Added

--- work/Splitter/script.js
+++ work/Splitter/script.js
@@ -1,10 +1,8 @@
 var tip;
-var tipVal;
+var tipVal = 0;
 var bill;
 var pax;
 var thisButton;
-
-
 
 function addTip(amount,buttonNum){
     tipVal = Number(amount);
@@ -14,8 +12,6 @@ function addTip(amount,buttonNum){
     return tipVal;
 };
 
-
-
 function calculateTip(){
     var bill = Number(document.getElementById("billInput").value);
     var pax = Number(document.getElementById("peopleInput").value);
@@ -23,19 +19,29 @@ function calculateTip(){
     
     if(bill <= 0 || pax <= 0){
         alert("input fields cannot be zero!");
+
     }else if(isNaN(bill) || isNaN(pax)){
         alert("input must be a number");
-    }else{
 
-        var tipAmount = (bill * tip / pax);
-        var totalAmount = Number((bill / pax) + tipAmount).toFixed(2);
-        
+    }else if(tipVal == 0){
+        var totalAmount = Number((bill / pax)).toFixed(2);
         var tipOutput = document.getElementsByClassName("tipOutput")[0];
-        tipOutput.innerHTML = "$" + (tipAmount).toFixed(2);
-
+        tipOutput.innerHTML = "$00.00";
         var totalOutput = document.getElementsByClassName("tipOutput")[1];
         totalOutput.innerHTML = "$" + totalAmount;
+        var i;
+        for(i = 0; i < 2; i++){
+            document.getElementsByClassName("tipOutput")[i].style.color = "#26c2ad";
+            document.getElementsByClassName("tipOutput")[i].style.fontSize = "2.5vw";
+        }
 
+    }else{
+        var tipAmount = (bill * tip / pax);
+        var totalAmount = Number((bill / pax) + tipAmount).toFixed(2);
+        var tipOutput = document.getElementsByClassName("tipOutput")[0];
+        tipOutput.innerHTML = "$" + (tipAmount).toFixed(2);
+        var totalOutput = document.getElementsByClassName("tipOutput")[1];
+        totalOutput.innerHTML = "$" + totalAmount;
         var i;
         for(i = 0; i < 2; i++){
             document.getElementsByClassName("tipOutput")[i].style.color = "#26c2ad";
