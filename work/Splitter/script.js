de374e607e8acf5fe47b0fab789fdb4b0fdbc762
DaleWegerle <tarryncwegerle@gmail.com>
2021-10-08 11:41:10 +0200

    Splitter Logic And Responsive Front End

--- work/Splitter/script.js
+++ work/Splitter/script.js
@@ -1,16 +1,59 @@
+var tip;
+var tipVal;
+var bill;
+var pax;
+var thisButton;
+
+
+
 function addTip(amount,buttonNum){
-    const tip = amount;
-    const i = buttonNum;
-    document.getElementsByClassName("tipButton")[i].style.backgroundColor = "#000000";
-    return tip;
-}
+    tipVal = Number(amount);
+    var i = buttonNum;
+    thisButton = document.getElementsByClassName("tipButton")[i];
+    thisButton.style.backgroundColor = "#26c2ad";
+    return tipVal;
+};
 
 
 
 function calculateTip(){
-    const bill = document.getElementById("billInput").value;
-    const pax = document.getElementById("billInput").value;
-    const tipAmount = addTip();
-    alert(bill + pax + tipAmount);
+    var bill = Number(document.getElementById("billInput").value);
+    var pax = Number(document.getElementById("peopleInput").value);
+    var tip = tipVal;
+    
+    if(bill <= 0 || pax <= 0){
+        alert("input fields cannot be zero!");
+    }else if(isNaN(bill) || isNaN(pax)){
+        alert("input must be a number");
+    }else{
 
+        var tipAmount = (bill * tip / pax);
+        var totalAmount = Number((bill / pax) + tipAmount).toFixed(2);
+        
+        var tipOutput = document.getElementsByClassName("tipOutput")[0];
+        tipOutput.innerHTML = "$" + (tipAmount).toFixed(2);
+
+        var totalOutput = document.getElementsByClassName("tipOutput")[1];
+        totalOutput.innerHTML = "$" + totalAmount;
+
+        var i;
+        for(i = 0; i < 2; i++){
+            document.getElementsByClassName("tipOutput")[i].style.color = "#26c2ad";
+            document.getElementsByClassName("tipOutput")[i].style.fontSize = "2.5vw";
+        }
+    }
+}
+
+function reset(){
+    document.getElementById("billInput").value = "";
+    document.getElementById("peopleInput").value = "";
+    document.getElementsByClassName("tipOutput")[0].innerHTML = "$00.00";
+    document.getElementsByClassName("tipOutput")[1].innerHTML = "$00.00";
+    tipVal.value = "";
+    thisButton.style.backgroundColor = "#00474b";
+    var i;
+        for(i = 0; i < 2; i++){
+            document.getElementsByClassName("tipOutput")[i].style.color = "#26c2ad";
+            document.getElementsByClassName("tipOutput")[i].style.fontSize = "2.5vw";
+        }
 }
